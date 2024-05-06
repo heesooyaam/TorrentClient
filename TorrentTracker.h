7ace87461effea2970bd53264347cb533480843d
heesooyaam <138811809+heesooyaam@users.noreply.github.com>
2024-05-06 13:26:04 +0300

    Update TorrentTracker.h

--- TorrentTracker.h
+++ TorrentTracker.h
@@ -1,85 +1,254 @@
 #pragma once
 
-#include "peer.h"
-#include "torrent_file.h"
-#include "bencode.h"
-#include "FileParser.h"
 #include <string>
-#include <vector>
-#include <openssl/sha.h>
-#include <fstream>
-#include <variant>
-#include <list>
-#include <map>
-#include <sstream>
-#include <cpr/cpr.h>
+#include <chrono>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <fcntl.h>
+#include <cstring>
+#include <sys/poll.h>
+#include "byte_tools.h"
+#include <memory>
+#include <cassert>
 
-class TorrentTracker {
+/*
+ * Обертка над низкоуровневой структурой сокета.
+ */
+class TcpConnect {
 public:
-    /*
-     * url - адрес трекера, берется из поля announce в .torrent-файле
-     */
-    TorrentTracker(const std::string& url)
-    : url_(url)
+    TcpConnect(std::string ip, int port, std::chrono::milliseconds connectTimeout, std::chrono::milliseconds readTimeout)
+            : ip_(ip)
+            , port_(port)
+            , connectTimeout_(connectTimeout)
+            , readTimeout_(readTimeout)
+            , closed(true)
     {}
 
-    /*
-     * Получить список пиров у трекера и сохранить его для дальнейшей работы.
-     * Запрос пиров происходит посредством HTTP GET запроса, данные передаются в формате bencode.
-     * Такой же формат использовался в .torrent файле.
-     * Подсказка: посмотрите, что было написано в main.cpp в домашнем задании torrent-file
-     *
-     * tf: структура с разобранными данными из .torrent файла из предыдущего домашнего задания.
-     * peerId: id, под которым представляется наш клиент.
-     * port: порт, на котором наш клиент будет слушать входящие соединения (пока что мы не слушаем и на этот порт никто
-     *  не сможет подключиться).
-     */
-    void UpdatePeers(const TorrentFile& tf, std::string peerId, int port)
+    ~TcpConnect()
     {
-        cpr::Response Resp = cpr::Get(
-                cpr::Url{url_},
-                cpr::Parameters {
-                        {"info_hash", tf.infoHash},
-                        {"peer_id", peerId},
-                        {"port", std::to_string(port)},
-                        {"uploaded", std::to_string(0)},
-                        {"downloaded", std::to_string(0)},
-                        {"left", std::to_string(tf.length)},
-                        {"compact", std::to_string(1)}
-                },
-                cpr::Timeout{20000}
-        );
-
-        if(Resp.status_code != 200)
-        {
-            std::cerr << "Something went wrong... Status code: " << Resp.status_code << std::endl;
-            return;
-        }
-
-        if(Resp.text.find("failure reason") != std::string::npos)
-        {
-            std::cerr << "Something went wrong... Text of responce: " << Resp.text << std::endl;
-            return;
-        }
-
-        std::stringstream stream;
-        stream << Resp.text;
-
-        auto Dict = *std::dynamic_pointer_cast<bencode::Dictionary>(bencode::parse(stream, (char) stream.get()));
-
-        peers_ = std::move(ParsePeer(std::dynamic_pointer_cast<bencode::String>(Dict["peers"])->get()));
+        CloseConnection();
     }
 
     /*
-     * Отдает полученный ранее список пиров
+     * Установить tcp соединение.
+     * Если соединение занимает более `connectTimeout` времени, то прервать подключение и выбросить исключение.
+     * Полезная информация:
+     * - https://man7.org/linux/man-pages/man7/socket.7.html
+     * - https://man7.org/linux/man-pages/man2/connect.2.html
+     * - https://man7.org/linux/man-pages/man2/fcntl.2.html (чтобы включить неблокирующий режим работы операций)
+     * - https://man7.org/linux/man-pages/man2/select.2.html
+     * - https://man7.org/linux/man-pages/man2/setsockopt.2.html
+     * - https://man7.org/linux/man-pages/man2/close.2.html
+     * - https://man7.org/linux/man-pages/man3/errno.3.html
+     * - https://man7.org/linux/man-pages/man3/strerror.3.html
      */
-    const std::vector<Peer>& GetPeers() const
+    void EstablishConnection()
     {
-        return peers_;
+        sock_ = socket(AF_INET, SOCK_STREAM, 0);
+        if(sock_ == -1)
+        {
+            throw std::runtime_error("Error: cannot create socket:(");
+        }
+
+        closed = true;
+
+        sockaddr_in sock_address;
+        memset(&sock_address, '0', sizeof(sock_address));
+        sock_address.sin_family = AF_INET;
+        sock_address.sin_addr.s_addr = inet_addr(ip_.c_str());
+        sock_address.sin_port = htons(port_);
+
+        int default_flags = fcntl(sock_, F_GETFL, 0);
+        set_nonblock_mode(default_flags);
+
+        int connection_result = connect(sock_, (const sockaddr*) &sock_address, sizeof(sock_address));
+
+        if(connection_result == 0)
+        {
+            set_default_mode(default_flags);
+            return;
+        }
+
+        pollfd arr;
+        memset(&arr, '0', sizeof(arr));
+        arr =
+                {
+                        .fd = sock_,
+                        .events = POLLOUT
+                };
+
+        connection_result = poll(&arr, 1, connectTimeout_.count());
+
+        if(connection_result == -1)
+        {
+            set_default_mode(default_flags);
+            throw std::runtime_error("Error: something went wrong...");
+        }
+        else if(connection_result == 0)
+        {
+            set_default_mode(default_flags);
+            throw std::runtime_error("Error: time limit exceeded");
+        }
+
+        set_default_mode(default_flags);
+    }
+    void set_default_mode(int default_flags) const
+    {
+        fcntl(sock_, F_SETFL, default_flags);
+        if(fcntl(sock_, F_GETFL, 0) != (default_flags))
+        {
+            throw std::runtime_error("Error: cannot set default mode");
+        }
+    }
+    void set_nonblock_mode(int current_flags) const
+    {
+        fcntl(sock_, F_SETFL, current_flags | O_NONBLOCK);
+        if(fcntl(sock_, F_GETFL, 0) != (current_flags | O_NONBLOCK))
+        {
+            throw std::runtime_error("Error: cannot set O_NONBLOCK mode");
+        }
+    }
+    /*
+     * Послать данные в сокет
+     * Полезная информация:
+     * - https://man7.org/linux/man-pages/man2/send.2.html
+     */
+    void SendData(const std::string& data) const
+    {
+        if(sock_ < 0)
+        {
+            throw std::runtime_error("Error: bad socket");
+        }
+
+        auto c_data = data.c_str();
+        size_t notSent = data.size();
+        while(notSent > 0)
+        {
+            int curSession = send(sock_, c_data, notSent, 0);
+
+            if(curSession < 0)
+            {
+                throw std::runtime_error("Error: cannot send data");
+            }
+
+            notSent -= curSession;
+            c_data += curSession;
+        }
     }
 
+    /*
+     * Прочитать данные из сокета.
+     * Если передан `bufferSize`, то прочитать `bufferSize` байт.
+     * Если параметр `bufferSize` не передан, то сначала прочитать 4 байта, а затем прочитать количество байт, равное
+     * прочитанному значению.
+     * Первые 4 байта (в которых хранится длина сообщения) интерпретируются как целое число в формате big endian,
+     * см https://wiki.theory.org/BitTorrentSpecification#Data_Types
+     * Полезная информация:
+     * - https://man7.org/linux/man-pages/man2/poll.2.html
+     * - https://man7.org/linux/man-pages/man2/recv.2.html
+     */
+    std::string ReceiveData(size_t bufferSize = 0) const
+    {
+        if(sock_ < 0)
+        {
+            throw std::runtime_error("Error: socket is not connected");
+        }
+
+        timeval TimeOut =
+                {
+                    .tv_sec = readTimeout_.count() / 1000,
+                    .tv_usec = readTimeout_.count() % 1000 * 1000
+                };
+        timeval DefaultTimeOut =
+                {
+                        .tv_sec = 0,
+                        .tv_usec = 0
+                };
+
+        setsockopt(sock_, SOL_SOCKET, SO_RCVTIMEO, (const char*) &TimeOut, sizeof(TimeOut));
+
+        if(bufferSize == 0)
+        {
+            char size[4];
+            char* ptr = size;
+            size_t needToGet = 4;
+            while(needToGet > 0)
+            {
+                int curSession = recv(sock_, ptr, needToGet, 0);
+
+                if(curSession <= 0)
+                {
+                    setsockopt(sock_, SOL_SOCKET, SO_RCVTIMEO, (const char*) &DefaultTimeOut, sizeof(DefaultTimeOut));
+                    throw std::runtime_error("Error: cannot send data");
+                }
+
+                needToGet -= curSession;
+                ptr += curSession;
+            }
+            std::string sz = {size[0], size[1], size[2], size[3]};
+            bufferSize = BytesToInt(sz);
+        }
+        char* buffer(new char[bufferSize]);
+        char* ptr = buffer;
+        size_t needToGet = bufferSize;
+        while(needToGet > 0)
+        {
+            int curSession = recv(sock_, ptr, needToGet, 0);
+
+            if(curSession <= 0)
+            {
+                delete[] buffer;
+                setsockopt(sock_, SOL_SOCKET, SO_RCVTIMEO, (const char*) &DefaultTimeOut, sizeof(DefaultTimeOut));
+                throw std::runtime_error("Error: cannot send data");
+            }
+
+            needToGet -= curSession;
+            ptr += curSession;
+        }
+
+        std::string res;
+
+        for(size_t i = 0; i < bufferSize; ++i)
+        {
+            res.push_back(buffer[i]);
+        }
+
+        delete[] buffer;
+        setsockopt(sock_, SOL_SOCKET, SO_RCVTIMEO, (const char*) &DefaultTimeOut, sizeof(DefaultTimeOut));
+        return res;
+    }
+
+    /*
+     * Закрыть сокет
+     */
+    void CloseConnection()
+    {
+        if(!closed)
+        {
+            closed = true;
+            close(sock_);
+        }
+    }
+
+    const std::string& GetIp() const
+    {
+        return ip_;
+    }
+    int GetPort() const
+    {
+        return port_;
+    }
 private:
-    std::string url_;
-    std::vector<Peer> peers_;
+    const std::string ip_;
+    const int port_;
+    std::chrono::milliseconds connectTimeout_, readTimeout_;
+    int sock_;
+    bool closed;
 };
-
