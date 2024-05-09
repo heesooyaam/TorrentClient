72e644aac078041a061c3164e724f54ed19506f3
heesooyaam <138811809+heesooyaam@users.noreply.github.com>
2024-05-10 01:34:38 +0300

    Update peer_connect.h

--- peer_connect.h
+++ peer_connect.h
@@ -3,15 +3,12 @@
 #include "tcp_connect.h"
 #include "peer.h"
 #include "torrent_file.h"
-#include <bit>
-#include <chrono>
+#include "piece_storage.h"
 
-using namespace std::chrono_literals;
 /*
  * Структура, хранящая информацию о доступности частей скачиваемого файла у данного пира
  */
-class PeerPiecesAvailability
-{
+class PeerPiecesAvailability {
 public:
     PeerPiecesAvailability() = default;
 
@@ -19,45 +16,33 @@ public:
      * bitfield -- массив байтов, в котором i-й бит означает наличие или отсутствие i-й части файла у пира
      * https://wiki.theory.org/BitTorrentSpecification#bitfield:_.3Clen.3D0001.2BX.3E.3Cid.3D5.3E.3Cbitfield.3E
      */
-    explicit PeerPiecesAvailability(std::string bitfield)
-    : bitfield_(bitfield)
-    {}
+    explicit PeerPiecesAvailability(std::string bitfield);
 
     /*
-     * Есть ли часть под номером `pieceIndex` у пира?
+     * Если ли часть под номером `pieceIndex` у пира?
      */
-    bool IsPieceAvailable(size_t pieceIndex) const
-    {
-        return bitfield_[pieceIndex / 8] & (1 << (7 - pieceIndex % 8));
-    }
+    bool IsPieceAvailable(size_t pieceIndex) const;
 
     /*
      * Пометить часть под номером `pieceIndex` как доступную
      */
-    void SetPieceAvailability(size_t pieceIndex)
-    {
-        bitfield_[pieceIndex / 8] |= (1 << (7 - pieceIndex % 8));
-    }
+    void SetPieceAvailability(size_t pieceIndex);
 
     /*
      * Сколько бит хранится в bitfield'е
      */
-    size_t Size() const
-    {
-        size_t cnt = 0;
-        for(auto&& byte : bitfield_)
-        {
-            cnt += std::__popcount(byte);
-        }
-        return cnt;
-    }
+    size_t Size() const;
 private:
     std::string bitfield_;
 };
 
+/*
+ * Класс, представляющий соединение с одним пиром.
+ * С помощью него можно подключиться к пиру и обмениваться с ним сообщениями
+ */
 class PeerConnect {
 public:
-    PeerConnect(const Peer& peer, const TorrentFile& tf, std::string selfPeerId);
+    PeerConnect(const Peer& peer, const TorrentFile& tf, std::string selfPeerId, PieceStorage& pieceStorage);
 
     /*
      * Основная функция, в которой будет происходить цикл общения с пиром.
@@ -66,7 +51,7 @@ public:
     void Run();
 
     void Terminate();
-protected:
+private:
     const TorrentFile& tf_;
     TcpConnect socket_;  // tcp-соединение с пиром
     const std::string selfPeerId_;  // наш id, которым представляется наш клиент
@@ -74,6 +59,9 @@ protected:
     PeerPiecesAvailability piecesAvailability_;
     bool terminated_;  // флаг, необходимый для завершения цикла общения с пиром
     bool choked_;  // https://wiki.theory.org/BitTorrentSpecification#Overview
+    PiecePtr pieceInProgress_;  // часть файла, с которой сейчас работаем
+    PieceStorage& pieceStorage_;  // отсюда берем новые части для скачивания
+    bool pendingBlock_;  // уже послали запрос на скачивание части файла и ждем ответ
 
     /*
      * Функция производит handshake.
@@ -106,5 +94,17 @@ protected:
      */
     void SendInterested();
 
-    virtual void MainLoop();
+    /*
+     * Функция отправляет пиру сообщение типа request. Это сообщение обозначает запрос части файла у пира.
+     * За одно сообщение запрашивается не часть целиком, а блок данных размером 2^14 байт или меньше.
+     * Если в данный момент мы не знаем, какую часть файла надо запросить у пира, то надо получить эту информацию у
+     * PieceStorage
+     */
+    void RequestPiece();
+
+    /*
+     * Основной цикл общения с пиром. Здесь мы ждем следующее сообщение от пира и обрабатываем его.
+     * Также, если мы не ждем в данный момент от пира содержимого части файла, то надо отправить соответствующий запрос
+     */
+    void MainLoop();
 };
